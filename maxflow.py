2ac6493a0254f8c2a8b38c79b1e560d5afa2b9ef
Alexandria Mullen <38265823+AlexandriaMullen@users.noreply.github.com>
2020-02-25 19:46:50 -0500

    Update maxflow.py

--- maxflow.py
+++ maxflow.py
@@ -4,16 +4,15 @@ Maximum flow (and minimum cut) algorithms on capacitated graphs.
 import networkx as nx
 from networkx.algorithms.flow.utils import *
 
-# Define the default flow function for computing maximum flow.
 import numpy
 
-__all__ = ['maximum_flow',
-           'maximum_flow_value',
-           'minimum_cut',
-           'minimum_cut_value']
+__all__ = ['dijkstra_maximum_flow',
+           'dijkstra_maximum_flow_value',
+           'dijkstra_minimum_cut',
+           'dijkstra_minimum_cut_value']
 
 
-def maximum_flow(G, s, t, capacity='capacity', flow_func=None, **kwargs):
+def dijkstra_maximum_flow(G, s, t, capacity='capacity', flow_func=None, **kwargs):
 
     if flow_func is None:
         if kwargs:
@@ -30,7 +29,7 @@ def maximum_flow(G, s, t, capacity='capacity', flow_func=None, **kwargs):
     return (R.graph['flow_value'], flow_dict)
 
 
-def maximum_flow_value(G, s, t, capacity='capacity', flow_func=None, **kwargs):
+def dijkstra_maximum_flow_value(G, s, t, capacity='capacity', flow_func=None, **kwargs):
 
     if flow_func is None:
         if kwargs:
@@ -46,7 +45,7 @@ def maximum_flow_value(G, s, t, capacity='capacity', flow_func=None, **kwargs):
     return R.graph['flow_value']
 
 
-def minimum_cut(G, s, t, capacity='capacity', flow_func=None, **kwargs):
+def dijkstra_minimum_cut(G, s, t, capacity='capacity', flow_func=None, **kwargs):
 
     if flow_func is None:
         if kwargs:
@@ -57,29 +56,21 @@ def minimum_cut(G, s, t, capacity='capacity', flow_func=None, **kwargs):
     if not callable(flow_func):
         raise nx.NetworkXError("flow_func has to be callable.")
 
-    if (kwargs.get('cutoff') is not None and
-        flow_func in (edmonds_karp, preflow_push, shortest_augmenting_path)):
-        raise nx.NetworkXError("cutoff should not be specified.")
-
     R = flow_func(G, s, t, capacity=capacity, value_only=True, **kwargs)
-    # Remove saturated edges from the residual network
+    
     cutset = [(u, v, d) for u, v, d in R.edges(data=True)
               if d['flow'] == d['capacity']]
     R.remove_edges_from(cutset)
 
-    # Then, reachable and non reachable nodes from source in the
-    # residual network form the node partition that defines
-    # the minimum cut.
     non_reachable = set(nx.shortest_path_length(R, target=t))
     partition = (set(G) - non_reachable, non_reachable)
-    # Finaly add again cutset edges to the residual network to make
-    # sure that it is reusable.
+
     if cutset is not None:
         R.add_edges_from(cutset)
     return (R.graph['flow_value'], partition)
 
 
-def minimum_cut_value(G, s, t, capacity='capacity', flow_func=None, **kwargs):
+def dijkstra_minimum_cut_value(G, s, t, capacity='capacity', flow_func=None, **kwargs):
     
     if flow_func is None:
         if kwargs:
