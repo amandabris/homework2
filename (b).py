b84f74a0a57856e3c24d04739e1df4ceb4d7fddc
Amanda Bris <bris@bc.edu>
2020-02-25 19:35:20 -0500

    adding font size and labels

--- (b).py
+++ (b).py
@@ -4,7 +4,7 @@ import networkx as nx
 
 G=nx.Graph()
 G=nx.read_edgelist("inputfile.edgelist",create_using=nx.DiGraph())
+nx.draw(G,with_labels = True,node_size=700,font_size=12)
 
-nx.draw(G)
 plt.show()
 print(G.nodes)
