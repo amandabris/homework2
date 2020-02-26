14b348a03d736ba211c9882671d564300de6165f
Amanda Bris <bris@bc.edu>
2020-02-25 19:47:44 -0500

    cleaned up

--- (b).py
+++ (b).py
@@ -1,10 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import networkx as nx
 
 G=nx.Graph()
 G=nx.read_edgelist("inputfile.edgelist",create_using=nx.DiGraph())
 nx.draw(G,with_labels = True,node_size=700,font_size=12)
 
-plt.show()
 print(G.nodes)
